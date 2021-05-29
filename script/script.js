ac57c56e845a0fac5e787e5370bbfd389133b442
Leo Art <artleo@mail.ru>
2021-05-29 09:15:44 +0300

    Объявил указанные в ДЗ переменные и вывел необходимую информацию

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
-let a = 10;
-const b = 'qwerty';
-alert('Some text...');
-alert('Some text...');
-console.log('And more some text'); // коммент
+let money; // пока речь на шла о типах и значениях, надеюсь правильно))
+let incom;
+let addExpenses;
+let deposit = false; //подсмотрел назначение в следующем задании
+let mission;
+let period;
+alert('сообщение с любым текстом в алерт');
+console.log('сообщение с любым текстом в консоль'); 
