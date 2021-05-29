2c726ae7ecfab68b566e59b1be356067978a8e35
Leo Art <artleo@mail.ru>
2021-05-29 11:52:31 +0300

    выполнил усложненный урок 2

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
-let money = 500; // усiм папиццот!))
-let incom = 'ремонты';
-let addExpenses = 'Коммуналка, Интернет, Телефоны, Обслуживание авто, Обучение ребенка, ЕДА';
-let deposit = false; //подсмотрел назначение в следующем задании
-let mission = 10000;
-let period = 12;
+const num = 266219;
+//let res = num.toString();
+//console.log(res);
+//num.toString();
 
-console.log('Тип money: ' + typeof (money)); 
-console.log('Тип incom: ' + typeof (incom)); 
-console.log('Тип deposit: ' + typeof (deposit)); 
-console.log('Длина строки addExpenses: ' + addExpenses.length + ' симв.'); // сокращал чтоб не морочиться с окончаниями)))
-console.log('Период равен ' + period + ' мес. \nЦель заработать $' + mission);
 
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(', '); 
-console.log(addExpenses); // если исходную строку менять не нужно,s то console.log(addExpenses.toLowerCase().split(', ')); 
+let result = 1;
+for (i =0; i < num.toString().length; i++) {
+    result *= +num.toString()[i];
+    //console.log(+num.toString()[i], typeof (+num.toString()[i]));
+}// другие методы перебора мне пока не известны, гуглить было некогда...
+console.log(result);
 
-let budgetDay = money / 30;
-console.log('Дневной бюджет: $' + budgetDay.toFixed(2));
+result = result ** 3;
+console.log(result);// не совсем понятно, в каком виде выводить две первые цифры...
+
+console.log(+result.toString().substr(0, 2)); //получили число из 2-х первых цифр
+console.log(result.toString().substr(0, 2).split('')); //если достаточно иметь цифры в виде массива строк
+console.log(+result.toString()[0], +result.toString()[1]); //может так правильно?
