e2ae4b524bb52c3abedb2c31f513424b2cc3838d
Leo Art <artleo@mail.ru>
2021-06-03 02:21:35 +0300

    сделал 4 и 5 вместе

--- script/script.js
+++ script/script.js
@@ -1,37 +1,30 @@
-let money = +prompt('Ваш месячный доход (р.):', '100000'); 
-//проверим, число ли?
-while (!money) {
-    alert('Введены некорректные данные! Введите число!')
-    money = +prompt('Ваш месячный доход (р.):', '100000');
-}
+let isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite (n);
+};
+
+let money;
 let income = 'ремонты';
 
 
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Коммуальные платежи, еда, другое');
-//если нажата "отмена", считаем, что расходов нет... Далее проверки не делал, т.к. они похожи (да и в задании не сказано) 
-if (!addExpenses) {
-    addExpenses = '';
-}
-
-
-/*7) Почистить консоль логи и добавить недостающие, должны остаться:
-
- - вызовы функции showTypeOf
-
- - Расходы за месяц вызов getExpensesMonth
-
- - Вывод возможных расходов в виде массива (addExpenses)
-
- - Cрок достижения цели в месяцах (результат вызова функции getTargetMonth) 
-
- - Бюджет на день (budgetDay)*/
-
- 
-
-
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'кино, теАтр');
 let deposit = confirm ('Есть ли у вас депозит в банке (если да, нажмите "OK")?');
 let mission = 1000000;//не было заданием спросить у пользователя, поэтому так и оставим
 let period = 12;
+//переписать с do..while
+let start = function() {
+    do {
+        money = +prompt('Ваш месячный доход (р.):', '100000'); // упрощает проверку на мой взгляд
+        //при таком вводе в условие цикла достаточно написать !money без использования дополнительной функции
+        // в констекте поставленной задачи
+        if (!isNumber(money)){
+            alert('Введены некорректные данные! Введите число!');
+        }
+    } while (!isNumber(money));
+    //нет проверки, если пользователь использовал в качестве разделителя вместо точки запятую,
+    //хотя у Макса возможно это тоже не реализовано... 
+};
+
+start();
 const showTypeOf = function(variable){
     console.log('Тип переменной, которая хранит ' + variable + ' - ' + typeof (variable)); 
 };
@@ -43,10 +36,10 @@ showTypeOf(deposit);
 console.log('Период равен ' + period + ' мес. \nЦель заработать ' + mission + ' р.');
 
 addExpenses = addExpenses.toLowerCase();
-addExpenses.split(', ');
+addExpenses = addExpenses.split(', ');
 console.log(addExpenses);  
 
-//1) Объявить функцию getExpensesMonth. Функция возвращает сумму всех (ВСЕХ, Карл!) обязательных расходов за месяц
+//1) Объявить функцию getExpensesMonth. 
 /* эта функция не катит...
 function getExpensesMonth(expense) {
     let indexOfSeparator = expense.lastIndexOf(',', expense.length);
@@ -60,20 +53,53 @@ function getExpensesMonth(expense) {
     }// если использовать addExpenses уже приведенную к неижнему регистру, то после трим тулоуэркейс не нужен
   } //Почти красивая реккурсия))), но не чистая...*/
 
-function getExpensesMonth(expenses){
-    let amountOfExpenses, amount;
-    for (let i = 0; i < expenses.length; i++){
+  let expenses = [];
+  function getExpensesMonth(){
+    let amountOfExpenses = 0, amount, i = 0;
+    while (true){//проверка ввода расходов, отмена - выход
+        do{
+            expenses[i] = prompt('Введите обязательную статью расходов (если все перечислили - нажмите "Отмена"):');
+            if (expenses[i] === ''){
+                alert ('Строка не должна быть пустой');
+                continue;
+            }  
+            if (expenses[i] === null){
+                return amountOfExpenses;
+            } 
+            
+        } while(!expenses[i]);
         do {
             amount = +prompt('Во сколько обходится (обходятся) ' + (expenses[i]) +' ?');
-            if (amount === NaN){
-                alert ('Введены некорректные данные! Введите число!');
+            //специально не использовал isNumber(), тоже работает благодаря строчке выше
+            if (isNaN(amount) || (amount === 0)){
+               alert ('Введены некорректные данные! Введите число!');
             }
-        } while (amount === NaN);
+        } while (isNaN(amount) || amount === 0);
+        i++;
         amountOfExpenses += amount;
     }
-    return amountOfExpenses;
 }
 
+
+ /*   let amountOfExpenses = 0, amount;
+    for (let i = 0; i < 4; i++){
+        expenses[i] = prompt('Введите обязательную статью расходов:');
+        do {
+            amount = +prompt('Во сколько обходится (обходятся) ' + (expenses[i]) +' ?');
+            //специально не использовал isNumber(), тоже работает благодаря строчке выше
+            if (isNaN(amount) || (amount === 0)){
+               alert ('Введены некорректные данные! Введите число!');
+            }
+        } while (isNaN(amount) || amount === 0);
+
+        amountOfExpenses += amount;
+    }
+    return amountOfExpenses;*/
+    
+console.log(expenses);
+// ВЫВОЖУ ЗНАЧЕНИЕ ФУНКЦИИ getExpensesMonth В КОНСОЛЬ:
+let expensesAmount = getExpensesMonth();// в уроке также через переменную, иначе повторный вызов, т.к. функция не чистая
+console.log('Расходы за месяц: ' + expensesAmount);
 //2) Объявить функцию getAccumulatedMonth. Функция возвращает Накопления за месяц (Доходы минус расходы)
 
 const getAccumulatedMonth = function(incomes, expenses){
@@ -81,7 +107,7 @@ const getAccumulatedMonth = function(incomes, expenses){
 };
 
 //3) Объявить переменную accumulatedMonth и присвоить ей результат вызова функции getAccumulatedMonth 
-let accumulatedMonth = getAccumulatedMonth (money, getExpensesMonth(addExpenses));
+let accumulatedMonth = getAccumulatedMonth (money, expensesAmount);
 console.log('Месячный бюджет: ' + accumulatedMonth + ' р.');
 
 //4) Объявить функцию getTargetMonth. Подсчитывает за какой период будет достигнута цель, зная результат месячного накопления (accumulatedMonth) и возвращает результат
@@ -113,4 +139,3 @@ const getStatusIncome = function(budget){
     
 }
 console.log(getStatusIncome(budgetDay));
-
